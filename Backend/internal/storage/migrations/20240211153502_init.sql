b8b626e9e81fa32b90c5ccd8e98b6b319e05154f
qrave1 <liveartem@yandex.ru>
2024-02-17 13:52:43 +0300

    add init migration

--- Backend/internal/storage/migrations/20240211153502_init.sql
+++ Backend/internal/storage/migrations/20240211153502_init.sql
@@ -7,7 +7,7 @@ CREATE TABLE users
     user_id  serial PRIMARY KEY,
     username varchar(50)  NOT NULL,
     email    VARCHAR(50)  NOT NULL,
-    password varchar(100) NOT NULL
+    password varchar(128) NOT NULL
 );
 
 CREATE TABLE folders
@@ -25,11 +25,12 @@ CREATE TABLE folders
 
 CREATE TABLE notes
 (
+    note_id  serial PRIMARY KEY,
     title    varchar(50) not null,
     body     text        not null,
-    group_id integer,
+    folder_id integer,
     CONSTRAINT fk_note_group
-        FOREIGN KEY (group_id)
+        FOREIGN KEY (folder_id)
             REFERENCES folders (folder_id)
             ON DELETE CASCADE
             ON UPDATE CASCADE
